4a973941f86ed558460b0e579458713b696e97a9
HuynhThoa <huynhthoa.sugr@gmail.com>
2023-03-25 18:49:50 +0700

    add page market- edit footer

--- controllers/User.php
+++ controllers/User.php
@@ -2,8 +2,14 @@
 
 	class User extends Controller{
 
-		// url là /account/index sẽ vào function này
+		// url là /User/index sẽ vào function này
 		public function Index()
+		{
+			return $this -> ReturnView(null,true); // Tham số đầu tiên là giá trị truyền lên view, tham số thứ hai là có sử dụng layout, nếu true là sử dụng layout main, ngược lại là không
+		}
+
+		// url là /User/Market sẽ vào function này
+		public function Market()
 		{
 			return $this -> ReturnView(null,true); // Tham số đầu tiên là giá trị truyền lên view, tham số thứ hai là có sử dụng layout, nếu true là sử dụng layout main, ngược lại là không
 		}
--- views/main.php
+++ views/main.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Main Layout</title>
-	<!-- basic -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Main Layout</title>
+    <!-- basic -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <!-- mobile metas -->
@@ -30,7 +31,7 @@
 
 
 <body class="main-layout">
-<header>
+    <header>
         <!-- header inner -->
         <div class="head-top">
             <div class="container-fluid">
@@ -64,100 +65,17 @@
             </div>
         </div>
     </header>
-		<?php require($view)?>
+    <?php require($view)?>
 
-        <footer>
+    <footer>
         <div class="footer">
-            <div class="container">
-                <div class="row">
+            <div class=" copyright text_align_left ">
+
+               <div class="row">
                     <div class="col-md-12">
-                        <a class="logo_bottom" href="index.html">BitCYPO</a>
+                        <a class="logo_bottom" href="index.html">SoloWallet</a>
                     </div>
-                    <div class="col-md-3 col-sm-6" ">
-                  <div class=" Informa conta ">
-                     <h3>Contact Us</h3>
-                     <ul>
-                        <li> <a href=" Javascript:void(0) "> <i class=" fa fa-map-marker " aria-hidden=" true "></i> Location
-                           </a>
-                        </li>
-                        <li> <a href=" Javascript:void(0) "><i class=" fa fa-phone " aria-hidden=" true "></i> Call +01 1234567890
-                           </a>
-                        </li>
-                        <li> <a href=" Javascript:void(0) "> <i class=" fa fa-envelope " aria-hidden=" true "></i> demo@gmail.com
-                           </a>
-                        </li>
-                     </ul>
-                     <ul>
-                        <li>Readable content of                              
-                        </li>
-                        <li>a page when looking                           
-                        </li>
-                        <li>at its layoutreadable                          
-                        </li>
-                        <li>content of a page                             
-                        </li>
-                        <li>when looking at its                         
-                        </li>
-                        <li>layout                          
-                        </li>
-                     </ul>
-                  </div>
-               </div>
-               <div class=" col-md-3 col-sm-6 "">
-                        <div class="Informa helpful">
-                            <h3>Useful Link</h3>
-                            <ul>
-                                <li>Readable content of
-                                </li>
-                                <li>a page when looking
-                                </li>
-                                <li>at its layoutreadable
-                                </li>
-                                <li>content of a page
-                                </li>
-                                <li>when looking at its
-                                </li>
-                                <li>layout
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-md-3 col-sm-6" ">
-                  <div class=" Informa ">
-                     <h3>Offices</h3>
-                     <ul>
-                        <li>Readable content of                              
-                        </li>
-                        <li>a page when looking                           
-                        </li>
-                        <li>at its layoutreadable                          
-                        </li>
-                        <li>content of a page                             
-                        </li>
-                        <li>when looking at its                         
-                        </li>
-                        <li>layout                          
-                        </li>
-                     </ul>
-                  </div>
-               </div>
-               <div class=" col-md-3 col-sm-6 ">
-                  <div class=" Informa ">
-                     <h3>Newsletter</h3>
-                     <form class=" newslatter_form ">
-                        <input class=" ente " placeholder=" Enter your email " type=" text " name=" Enter your email ">
-                        <button class=" subs_btn ">Subscribe</button>
-                     </form>
-                  </div>
-               </div>
-            </div>
-            <div class=" copyright text_align_center ">
-               <div class=" container ">
-                  <div class=" row ">
-                     <div class=" col-md-12 ">
-                        <p>© 2020 All Rights Reserved. Design by   <a href=" https://html.design/ "> Free Html Template</a></p>
-                     </div>
-                  </div>
+                    <div class="col-md-3 col-sm-6" >
                </div>
             </div>
          </div>
