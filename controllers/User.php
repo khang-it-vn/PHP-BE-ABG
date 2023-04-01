1802c14969c385b3c1293151bf024c6584e0d0a5
Khang@22 <nguyenhoangkhangnsc19@gmail.com>
2023-04-01 22:39:15 +0700

    Update session-order

--- controllers/User.php
+++ controllers/User.php
@@ -2,34 +2,35 @@
 
 use Google\Service\Fitness\Session;
 
-	class User extends Controller{
+class User extends Controller
+{
 
 
-		// url là user/index 
-		public function Index()
-		{
-			return $this -> returnView(null,true);
-		}
+	// url là user/index 
+	public function Index()
+	{
+		return $this->returnView(null, true);
+	}
 
 
-		// url là /User/wallet sẽ vào function này
-		public function Wallet()
-		{
-			return $this -> ReturnView(null,true); // Tham số đầu tiên là giá trị truyền lên view, tham số thứ hai là có sử dụng layout, nếu true là sử dụng layout main, ngược lại là không
-		}
+	// url là /User/wallet sẽ vào function này
+	public function Wallet()
+	{
+		return $this->ReturnView(null, true); // Tham số đầu tiên là giá trị truyền lên view, tham số thứ hai là có sử dụng layout, nếu true là sử dụng layout main, ngược lại là không
+	}
 
-		// url là /User/Market sẽ vào function này
-		public function Market()
-		{
-			return $this -> ReturnView(null,true); // Tham số đầu tiên là giá trị truyền lên view, tham số thứ hai là có sử dụng layout, nếu true là sử dụng layout main, ngược lại là không
-		}
-		// url là /User/login
-		public function Login()
-		{
-			return $this -> ReturnView(null,true);
-		}
+	// url là /User/Market sẽ vào function này
+	public function Market()
+	{
+		return $this->ReturnView(null, true); // Tham số đầu tiên là giá trị truyền lên view, tham số thứ hai là có sử dụng layout, nếu true là sử dụng layout main, ngược lại là không
+	}
+	// url là /User/login
+	public function Login()
+	{
+		return $this->ReturnView(null, true);
+	}
 
-		/* ==> Version mowis kho con ho tro
+	/* ==> Version mowis kho con ho tro
 		
 
 		// url là /user/google
@@ -42,28 +43,83 @@ use Google\Service\Fitness\Session;
         }
 		*/
 
-		// url là user/checkaountfromgoogle
-		public function checkAccountFromGoogle()
+	// url là user/checkaountfromgoogle
+	public function checkAccountFromGoogle()
+	{
+		$body = array('credential' => $_POST['credential']);
+
+		$options = array(
+			'http' => array(
+				'method' => 'POST',
+				'header' => 'Content-type: application/x-www-form-urlencoded',
+				'content' => http_build_query($body)
+			)
+		);
+
+		$context  = stream_context_create($options);
+		$response = file_get_contents(E_LOGIN, false, $context);
+		$response = json_decode($response, true);
+
+		// print_r($response);
+		// detech token and role from response
+		SessionUtil::createSessionWithTokenRole($response[1]['token'], $response[2]['roles']['from']);
+		
+		// checkmpass
+		$status = $this->getInfo();
+
+		// mpass is null redirect view set product
+		if($status === false)
 		{
-			$body = array('credential' => $_POST['credential']);
-
-			$options = array(
-				'http' => array(
-					'method' => 'POST',
-					'header' => 'Content-type: application/x-www-form-urlencoded',
-					'content' => http_build_query($body)
-				)
-			);
-
-			$context  = stream_context_create($options);
-			$response = file_get_contents(E_LOGIN, false, $context);		
-			$response = json_decode($response, true);
-			
-			// detech token and role from response
-			SessionUtil::createSessionWithTokenRole($response[1],$response[2]['roles']['from']);
-			// check role and redirect to access page
-			SessionUtil::getRoleRedirect();
+			header("Location: ".ROOT_URL."user/updatempass");
+			return;
 		}
-	
+
+		// check role and redirect to access page
+		SessionUtil::getRoleRedirect();
 	}
-?>
+
+
+	public function getInfo()
+	{
+
+		$url = E_GET_INFO;
+		$token = SessionUtil::getToken();
+
+
+		$options = array(
+			'http' => array(
+				'method'  => 'GET',
+				'header'  => 'Authorization: Bearer ' . $token . "\r\n" .
+					'Content-Type: application/json'
+			)
+		);
+
+		$context = stream_context_create($options);
+		$response = file_get_contents($url, false, $context);
+		$response =	json_decode($response);
+
+		SessionUtil::setInfoToken($response[0]);
+		if(strlen($response[0] -> mpass) === 0)
+		{
+			return false;
+		}
+	}
+
+
+	public function updatempass()
+	{
+		$this ->updateMPassPost();
+		return $this ->returnView(null, true);
+	}
+
+	public function updateMPassPost()
+	{
+		if(isset($_POST['mpass']))
+		{
+
+		}
+	}
+
+	
+
+}
--- endpoint.php
+++ endpoint.php
@@ -9,6 +9,16 @@
     // method post
     // body: file{image}
     // res: filename
-    define("E_UPLOAD_IMAGE", E_BASE."util/upload")
+    define("E_UPLOAD_IMAGE", E_BASE."util/upload");
+
+
+    // using for update mpass
+    // method post
+    define("E_UPDATE_M_PASS",E_BASE . "update-m-pass");
+
+    
+    // using for get info user from token
+    // method post
+    define("E_GET_INFO",E_BASE . "getUserInfo");
 
 ?>
